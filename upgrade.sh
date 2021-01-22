1b397a59357b6d2998700496e0e276e763941bb3
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-01-22 17:56:58 +0100

    startup scripts tweaked

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ date +%Y-%m\(%B\)-%d\(%A\)\ %H:%M:%S\ Amsterdam.NL\ timezone > lastModified.when
 date +%Y-%m\(%B\)-%d\(%A\)\ %H:%M:%S\ Amsterdam.NL\ timezone > lastModified.whenJS.txt
 
 git fetch --all
-git reset --hard origin/master
+git reset --hard origin/main
 
 #cd nicerapp/3rd-party/php-couchdb
 #git fetch --all
