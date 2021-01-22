49440168888ef83b51e45d76f06635ec323f1370
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-01-22 18:23:04 +0100

    startup scripts tweaked

--- nicerapp/selfHealer/ajax_getReport.php
+++ nicerapp/selfHealer/ajax_getReport.php
@@ -1,5 +1,5 @@
 <?php 
     $ramDiskFolder = realpath(dirname(__FILE__).'/../../..').'/RAM_disk';
-    echo $ramDiskFolder;
+    //echo $ramDiskFolder;
     echo file_get_contents ($ramDiskFolder.'/report.html');
 ?>
