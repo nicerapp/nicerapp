17c65792ca42dae0e39382b03841a4619bbecce4
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-01-22 17:42:17 +0100

    startup scripts tweaked

--- nicerapp/selfHealer/run.php
+++ nicerapp/selfHealer/run.php
@@ -1,6 +1,6 @@
 <?php 
-    require_once (dirname(__FILE__).'/selfHealer-1.0.0.php');
-    require_once (dirname(__FILE__).'/../../functions.php');
+    require_once (dirname(__FILE__).'/class.selfHealer.php');
+    require_once (dirname(__FILE__).'/../functions.php');
     
     global $selfHealer;
     global $sh;
@@ -9,7 +9,7 @@
     
     while (true!==false) {
         $r = $selfHealer->run($report);
-        $RAMdisk = realpath(dirname(__FILE__).'/../../../../RAM_disk');
+        $RAMdisk = realpath(dirname(__FILE__).'/../../../RAM_disk');
         //$reportFilename = $RAMdisk.'/'.date('Ymd_His').'_report.html';
         $reportFilename = $RAMdisk.'/report.html';
         file_put_contents ($reportFilename, $report);
