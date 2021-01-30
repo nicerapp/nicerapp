55d8953adfae4b991b8028c8889a9cc520b147e5
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-01-31 00:17:42 +0100

    analytics geoIP retrieval now enabled

--- nicerapp/apps/nicerapp/analytics/app.source.js
+++ nicerapp/apps/nicerapp/analytics/app.source.js
@@ -480,7 +480,7 @@ na.analytics = {
         dateStr = x.toLocaleString('nl-NL',{timeZone:'CET'}) + '.' + x.getMilliseconds(),
         html = '<div class="datetimeLocal" style="position:absolute;top:'+evt.layerY+'px;left:'+evt.layerX+'px;background:rgba(0,0,0,0.8);border:3px ridge white;border-radius:5px;">'+dateStr+'</div>';
         
-        jQuery('#siteContent .vividDialogContent').prepend(html);
+        jQuery('#siteContent').prepend(html);
     },
     
     
@@ -498,7 +498,7 @@ na.analytics = {
                 var 
                 evt = na.analytics.settings.evt,
                 html = '<div class="geoIP" style="position:absolute;top:'+evt.layerY+'px;left:'+evt.layerX+'px;background:rgba(0,0,0,0.8);border:3px ridge white;border-radius:5px;">'+data+'</div>';
-                jQuery('#siteContent .vividDialogContent').append(html);
+                jQuery('#siteContent').append(html);
                 //debugger;
             }
         };
