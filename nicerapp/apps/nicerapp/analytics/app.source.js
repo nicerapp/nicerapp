6abb6ddf28c03d9c17c9722f12bb6d2570b0a4ad
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-01-31 09:49:28 +0100

    bughunt for geoip feature of analytics

--- nicerapp/apps/nicerapp/analytics/app.source.js
+++ nicerapp/apps/nicerapp/analytics/app.source.js
@@ -505,8 +505,8 @@ na.analytics = {
                 var 
                 evt = na.analytics.settings.evt,
                 html = '<div class="geoIP" style="position:absolute;top:'+evt.layerY+'px;left:'+evt.layerX+'px;background:rgba(0,0,0,0.8);border:3px ridge white;border-radius:5px;z-index:1200;">'+data+'</div>';
-                var div = $.html(html);
-                jQuery('#siteContent').prepend(div);
+                //var div = $.parseHTML(html);
+                jQuery('#siteContent').prepend(html);
                 //debugger;
             }
         };
