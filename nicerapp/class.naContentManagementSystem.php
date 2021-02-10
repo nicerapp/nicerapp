e432fbecdfe2a84673980ccf2d4581dcac50725d
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-02-10 09:43:25 +0100

    bugfix

--- nicerapp/class.naContentManagementSystem.php
+++ nicerapp/class.naContentManagementSystem.php
@@ -136,7 +136,7 @@ class nicerAppCMS {
             $file = str_replace ('{$domain}', $this->domain, $file);
             $url = str_replace ($this->basePath,'',$file);
             $search = array ('{$src}', '{$changed}');
-            $replace = array ($url, date('Ymd_His', filectime($this->basePath.'/'.$file)));
+            $replace = array ($url, date('Ymd_His', filemtime($this->basePath.'/'.$file)));
             $lines .= str_replace ($search, $replace, $lineSrc);
         };
         return $lines;
