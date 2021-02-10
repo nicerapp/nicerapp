93da50919263ee4b9f2409c7f073cb2d9c0f64c6
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-02-10 10:39:52 +0100

    bugfix

--- nicerapp/domainConfigs/nicerapp_v2/ajax_backgrounds.php
+++ nicerapp/domainConfigs/nicerapp_v2/ajax_backgrounds.php
@@ -1,10 +1,18 @@
 <?php 
     require_once(dirname(__FILE__).'/../../boot.php');
-    $root = realpath(dirname(__FILE__).'/../../siteMedia/backgrounds');
-    $files = getFilePathList ($root, true, '/.*/', array('file'));
-    $files2 = array();
-    foreach ($files as $idx => $file) {
-        $files2[] = str_replace ($root, '', $file);
+    $cacheFile = realpath(dirname(__FILE__).'/../../siteCache/backgrounds.json');
+    
+    if (!file_exists($cacheFile)) {
+        $root = realpath(dirname(__FILE__).'/../../siteMedia/backgrounds');
+        $files = getFilePathList ($root, true, '/.*/', array('file'));
+        $files2 = array();
+        foreach ($files as $idx => $file) {
+            $files2[] = str_replace ($root, '', $file);
+        }
+        $r = json_encode($files2, JSON_PRETTY_PRINT);
+        echo $r;
+        file_put_contents($cacheFile, $r);
+    } else {
+        echo file_get_contents($cacheFile);
     }
-    echo json_encode($files2, JSON_PRETTY_PRINT);
 ?>
