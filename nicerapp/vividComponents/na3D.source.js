dc44b9122c0342d9bb19d4d188d4e0e12cceb5b5
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-02-13 10:08:28 +0100

    3D model loading now fully works! :)

--- nicerapp/vividComponents/na3D.source.js
+++ nicerapp/vividComponents/na3D.source.js
@@ -1,4 +1,33 @@
+import {
+  AmbientLight,
+  AnimationMixer,
+  AxesHelper,
+  Box3,
+  Cache,
+  CubeTextureLoader,
+  DirectionalLight,
+  GridHelper,
+  HemisphereLight,
+  LinearEncoding,
+  LoaderUtils,
+  LoadingManager,
+  PMREMGenerator,
+  PerspectiveCamera,
+  RGBFormat,
+  Scene,
+  SkeletonHelper,
+  UnsignedByteType,
+  Vector3,
+  WebGLRenderer,
+  sRGBEncoding,
+} from '/nicerapp/3rd-party/3D/libs/three.js/build/three.module.js';
+import Stats from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/libs/stats.module.js';
 import { GLTFLoader } from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/loaders/GLTFLoader.js';
+import { KTX2Loader } from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/loaders/KTX2Loader.js';
+import { DRACOLoader } from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/loaders/DRACOLoader.js';
+import { OrbitControls } from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/controls/OrbitControls.js';
+import { RGBELoader } from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/loaders/RGBELoader.js';
+//import { GLTFLoader } from '/nicerapp/3rd-party/3D/libs/three.js/examples/jsm/loaders/GLTFLoader.js';
 
 export class na3D_fileBrowser {
     constructor(el,parent) {
@@ -7,25 +36,29 @@ export class na3D_fileBrowser {
         this.el = el;
         this.t = $(this.el).attr('theme');
         
+        this.lights = [];
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, $(el).width() / $(el).height(), 0.1, 1000 );
 
-        this.renderer = new THREE.WebGLRenderer({alpha:true});
+        this.renderer = new THREE.WebGLRenderer({alpha:true, antialias : true});
+        this.renderer.physicallyCorrectLights = true;
+        this.renderer.outputEncoding = sRGBEncoding;
+        this.renderer.setPixelRatio (window.devicePixelRatio);
         this.renderer.setSize( $(el).width()-20, $(el).height()-20 );
+        
+        this.renderer.toneMappingExposure = 1.0;
+        
         el.appendChild( this.renderer.domElement );
         
         this.loader = new GLTFLoader();
         
         this.loader.load( '/nicerapp/3rd-party/3D/models/001/scene.gltf', function ( gltf ) {
-            gltf.scene.position.x = -20;
-            gltf.scene.position.z = 550;
+            gltf.scene.position.x = -80;
+            gltf.scene.scale.setScalar (10);
             t.cube = gltf.scene;
             t.scene.add (t.cube);
             
-            const color = 0xFFFFFF;
-            const intensity = 300;
-            const light = new THREE.AmbientLight(color, intensity);
-            t.scene.add(light);
+            t.updateTextureEncoding(t, t.cube);
         }, function ( xhr ) {
             console.log( 'cube 1 : ' + ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
         }, function ( error ) {
@@ -36,15 +69,41 @@ export class na3D_fileBrowser {
             t.cube2 = gltf.scene;
             t.scene.add (t.cube2);
             
-            const color = 0xFFFFFF;
-            const intensity = 300;
-            const light = new THREE.AmbientLight(color, intensity);
-            t.scene.add(light);
+            t.updateTextureEncoding(t, t.cube2);
+            
         }, function ( xhr ) {
             console.log( 'cube 2 : ' + ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
         }, function ( error ) {
             console.error( error );
         } );
+        
+        
+        /*
+        const color = 0xFFFFFF;
+        const intensity = 0.3;
+        const light = new THREE.AmbientLight(color, intensity);
+        t.scene.add(light);
+        */
+        
+        const light1  = new AmbientLight(0xFFFFFF, 0.3);
+        light1.name = 'ambient_light';
+        light1.intensity = 0.3;
+        light1.color = 0xFFFFFF;
+        this.camera.add( light1 );
+
+        const light2  = new DirectionalLight(0xFFFFFF, 0.8 * Math.PI);
+        light2.position.set(0.5, 0, 0.866); // ~60º
+        light2.name = 'main_light';
+        light2.intensity = 0.8 * Math.PI;
+        light2.color = 0xFFFFFF;
+        this.camera.add( light2 );
+
+        this.lights.push(light1, light2);        
+        
+        this.pmremGenerator = new PMREMGenerator( this.renderer );
+        this.pmremGenerator.compileEquirectangularShader();
+        
+        this.updateEnvironment(this);
         
         $(el).bind('mousemove', function() { t.onMouseMove (event, t) });
         
@@ -59,6 +118,79 @@ export class na3D_fileBrowser {
         this.animate(this);
     }
     
+    updateTextureEncoding (t, content) {
+        /*const encoding = this.state.textureEncoding === 'sRGB'
+        ? sRGBEncoding
+        : LinearEncoding;*/
+        const encoding = sRGBEncoding;
+        t.traverseMaterials(content, (material) => {
+            if (material.map) material.map.encoding = encoding;
+            if (material.emissiveMap) material.emissiveMap.encoding = encoding;
+            if (material.map || material.emissiveMap) material.needsUpdate = true;
+        });
+    }
+    
+    traverseMaterials (object, callback) {
+        object.traverse((node) => {
+            if (!node.isMesh) return;
+            const materials = Array.isArray(node.material)
+                ? node.material
+                : [node.material];
+            materials.forEach(callback);
+        });
+    }
+    
+    updateEnvironment (t) {
+        /*
+        const environment = {
+            id: 'venice-sunset',
+            name: 'Venice Sunset',
+            path: '/nicerapp/3rd-party/3D/assets/environment/venice_sunset_1k.hdr',
+            format: '.hdr'
+        };*/
+        const environment = {
+            id: 'footprint-court',
+            name: 'Footprint Court (HDR Labs)',
+            path: '/nicerapp/3rd-party/3D/assets/environment/footprint_court_2k.hdr',
+            format: '.hdr'
+        }
+
+        t.getCubeMapTexture( environment ).then(( { envMap } ) => {
+
+            /*if ((!envMap || !this.state.background) && this.activeCamera === this.defaultCamera) {
+                t.scene.add(this.vignette);
+            } else {
+                t.scene.remove(this.vignette);
+            }*/
+
+            t.scene.environment = envMap;
+            //this.scene.background = this.state.background ? envMap : null;
+
+        });
+
+    }    
+    
+    getCubeMapTexture ( environment ) {
+        const { path } = environment;
+
+        // no envmap
+        if ( ! path ) return Promise.resolve( { envMap: null } );
+
+        return new Promise( ( resolve, reject ) => {
+            new RGBELoader()
+                .setDataType( UnsignedByteType )
+                .load( path, ( texture ) => {
+
+                    const envMap = this.pmremGenerator.fromEquirectangular( texture ).texture;
+                    this.pmremGenerator.dispose();
+
+                    resolve( { envMap } );
+
+                }, undefined, reject );
+        });
+    }
+
+    
     onMouseMove( event, t ) {
         var rect = t.renderer.domElement.getBoundingClientRect();
         t.mouse.x = ( ( event.clientX - rect.left ) / ( rect.width - rect.left ) ) * 2 - 1;
@@ -72,7 +204,7 @@ export class na3D_fileBrowser {
         t.raycaster.setFromCamera (t.mouse, t.camera);
         
         const intersects = t.raycaster.intersectObjects (t.scene.children, true);
-        if (intersects[0]) {
+        if (intersects[0] && t.cube && t.cube2) {
             t.cube.rotation.y += 0.02;
             t.cube2.rotation.y += 0.02;
         }
