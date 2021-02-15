67b08c8e1c7c3d1fd0ca9c158884d31ed0d847b4
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-02-15 19:13:54 +0100

    bugfix for scrollbars

--- nicerapp/domainConfigs/nicerapp_v2/mainmenu.items.apps-games.php
+++ nicerapp/domainConfigs/nicerapp_v2/mainmenu.items.apps-games.php
@@ -2,7 +2,7 @@
 <ul>
     <li><a class="linkToNewPage" href="/ " windowName="naFrontpage" alt="Frontpage">Front page</a></li>
     <li><a class="linkToNewPage" href="/apps/eyJtdXNpYyI6eyJzZXQiOiJpbmRleCJ9fQ" windowName="naMusic" alt="Music">Music</a></li>
-    <li><a class="linkToNewPage" href="/apps/eyJuZXdzIjoiRW5nbGlzaF9OZXdzIn0" windowName="naNews" alt="News headlines">News</a></li>
+    <li><a class="linkToNewPage" href="/apps/eyJuZXdzIjp7InNlY3Rpb24iOiJFbmdsaXNoX05ld3MifX0" windowName="naNews" alt="News headlines">News</a></li>
     <li><a class="linkToNewPage" href="/apps/eyJ0YXJvdCI6eyJkZWNrIjoiT3JpZ2luYWwtUmlkZXItV2FpdGUiLCJyZWFkaW5nIjoiMy1DYXJkcyJ9fQ" windowName="naTarot" alt="Tarot game">Tarot</a></li>
 </ul>
 </li>
