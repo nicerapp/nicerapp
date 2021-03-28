e38da12b7f71c9df0a59cad5a0b3f359f5c23310
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-03-28 16:27:40 +0200

    bugfix

--- nicerapp/vividComponents/na3D.source.js
+++ nicerapp/vividComponents/na3D.source.js
@@ -400,6 +400,7 @@ export class na3D_fileBrowser {
                 
                 for (var i=0; i<6; i++) {
                     var p = null;
+                    console.log (itd[''+i]);
                     if (itd[''+i] && itd[''+i].match(/.*\.png|.*\.jpg|.*\.gif/)) {
                         var 
                         p = t.items[it.parent],
