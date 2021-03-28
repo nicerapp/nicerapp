3ae8a827b5027f5f87e98bcfd02e4122fa039d0c
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-03-28 18:33:26 +0200

    bugfix

--- nicerapp/vividComponents/na3D.source.js
+++ nicerapp/vividComponents/na3D.source.js
@@ -892,9 +892,9 @@ export class na3D_fileBrowser {
                     
                     if (it.model) {
                         //it.model.position.x = (commonParentIt.model?commonParentIt.model.position.x:0) + (200 * ox.modifierColumn * (ox.modifierColumn===-1?it.adjustedModXmin:it.adjustedModXadd)) + it.modifierColumn * 50;
-                        it.model.position.x = (p1it.model?p1it.model.position.x:0) + (200 * ox.modifierColumn * (ox.modifierColumn===-1?it.adjustedModXmin:it.adjustedModXadd)) + it.modifierColumn * 50;
+                        it.model.position.x = (p1it.model?p1it.model.position.x:0) + (200 * ox.modifierColumn * (ox.modifierColumn===-1?it.adjustedModXmin:it.adjustedModXadd)) + (it.modifierColumn * 50) + (it.modifierColumn * it.column * 50);
                         //it.model.position.y = (commonParentIt.model?commonParentIt.model.position.y:0) + (200 * ox.modifierRow * (ox.modifierRow===-1?it.adjustedModYmin:it.adjustedModYadd)) + it.modifierRow * 50;
-                        it.model.position.y = (p1it.model?p1it.model.position.y:0) + (200 * ox.modifierRow * (ox.modifierRow===-1?it.adjustedModYmin:it.adjustedModYadd)) + it.modifierRow * 50;
+                        it.model.position.y = (p1it.model?p1it.model.position.y:0) + (200 * ox.modifierRow * (ox.modifierRow===-1?it.adjustedModYmin:it.adjustedModYadd)) + (it.modifierRow * 50) +  (it.modifierRow * it.row * 50);
                         //debugger;
                         
                         if (ox.modifiedColumn===1) {
