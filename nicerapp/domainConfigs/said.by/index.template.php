19de2271c884c2528a249f3bc77a3a9f0e156a4c
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-04-07 12:13:35 +0200

    improvements for said.by

--- nicerapp/domainConfigs/said.by/index.template.php
+++ nicerapp/domainConfigs/said.by/index.template.php
@@ -27,14 +27,6 @@ na.site.globals = {
     domain : '{$domain}'
 };
 </script>
-    <link rel="apple-touch-icon" sizes="180x180" href="/nicerapp/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/nicerapp/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/nicerapp/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/nicerapp/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/nicerapp/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    <!--
     <link rel="apple-touch-icon" sizes="180x180" href="/nicerapp/favicon.said.by/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/nicerapp/favicon.said.by/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/nicerapp/favicon.said.by/favicon-16x16.png">
@@ -44,7 +36,6 @@ na.site.globals = {
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-config" content="/nicerapp/favicon.said.by/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
-    -->
     <title>{$title}</title>
 </head>
 <body onload="nas.onload(event);">
