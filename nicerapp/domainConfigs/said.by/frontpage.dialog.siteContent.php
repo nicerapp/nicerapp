06ee135f01a265d77f509c67677a8082120c2b5d
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-04-19 00:08:04 +0200

    bughunt for said.by

--- nicerapp/domainConfigs/said.by/frontpage.dialog.siteContent.php
+++ nicerapp/domainConfigs/said.by/frontpage.dialog.siteContent.php
@@ -4,7 +4,7 @@
                     <table id="tableFor_saCompanyLogo" border="0" style="width:300px;">
                         <tr>
                             <td>
-                                <div id="divFor_saCompanyLogo" style="margin-left:40px;width:200px;height:200px;background:rgba(0,0,0,0.7);border-radius:10px;border:solid rgba(0,0,0,0.8);padding:5px;box-shadow:4px 4px 2px rgba(0,0,0,0.7);">
+                                <div id="divFor_saCompanyLogo" style="width:200px;height:200px;background:rgba(0,0,0,0.7);border-radius:10px;border:solid rgba(0,0,0,0.8);padding:5px;box-shadow:4px 4px 2px rgba(0,0,0,0.7);">
                                     <canvas id="saCompanyLogo" width="200" height="200" onclick="event.data={element:'saCompanyLogo'}; na.logo.settings.stage.removeAllChildren(); na.logo.init_do_createLogo('saCompanyLogo','countryOfOriginColors');"></canvas><!-- this version can not actually be resized yet. hardcoded values in the drawing functions. -->
                                     <script type="text/javascript">
                                         $(document).ready(function() {
