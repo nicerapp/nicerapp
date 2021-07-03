635c75e4fd85fdf07dd164683743861361b7c11f
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-03 13:43:52 +0200

    bugfixes

--- nicerapp/dialogSettings.js
+++ nicerapp/dialogSettings.js
@@ -157,6 +157,9 @@ na.ds = na.dialogSettings = {
         var s = JSON.parse( $('#specificity').find('option:selected')[0].value );
         na.ds.settings.current.specificity = s;
         
+        $('#btnViewResult .cvbBorderCSS').css({ boxShadow : '0px 0px 0px 0px rgba(0,0,0,0)' });
+        $('#btnViewResult .cvbImgButton').css({ top : 2 });
+        
         $('.vividButton, .vividButton_icon', $('#siteToolbarDialogSettings')[0]).each(function(idx,el){
             na.site.settings.buttons['#'+el.id] = new naVividButton(el);
         });
