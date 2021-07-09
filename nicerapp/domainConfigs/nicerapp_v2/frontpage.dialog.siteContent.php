581ad5b15d88ca5d694408b847e102f20b3a7809
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-09 22:31:04 +0200

    temporary measure 1a

--- nicerapp/domainConfigs/nicerapp_v2/frontpage.dialog.siteContent.php
+++ nicerapp/domainConfigs/nicerapp_v2/frontpage.dialog.siteContent.php
@@ -211,7 +211,7 @@
 
     <h2 class="contentSectionTitle2">Available Apps</h2>
 
-    <!--<a href="<?php echo $urls['cms'];?>"><h3>Blogging features</h3></a>.<br/>-->
+    <a href="<?php echo $urls['cms'];?>"><h3>Blogging features</h3></a>.<br/>
     <a href="https://said.by" target="saidBy"><h3>Blogging features (on https://said.by)</h3></a>
     
     <a href="https://zoned.at" target="zonedAt"><h3>URL redirection (on https://zoned.at)</h3></a>
