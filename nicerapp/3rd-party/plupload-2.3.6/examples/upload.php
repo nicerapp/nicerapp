4bfdec040a92adf36313199817d0bce77e281dbb
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-13 04:29:33 +0200

    bughunt2

--- nicerapp/3rd-party/plupload-2.3.6/examples/upload.php
+++ nicerapp/3rd-party/plupload-2.3.6/examples/upload.php
@@ -55,7 +55,7 @@ $debug = false;
 $targetDir = 
     //realpath( //MESSES THINGS UP
         dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'
-        .'/siteData/'.$cms->domain.DIRECTORY_SEPARATOR.$_GET['basePath']
+        .'/siteData/'.$cms->domain.DIRECTORY_SEPARATOR.$_GET['basePath'];
     //);
     
 //echo $targetDir; die();
