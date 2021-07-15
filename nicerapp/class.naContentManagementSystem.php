6d332ea1349fe0c62aa3e3461ca6b8912cca9e63
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-15 22:06:53 +0200

    bugfix for blogging photo upload features

--- nicerapp/class.naContentManagementSystem.php
+++ nicerapp/class.naContentManagementSystem.php
@@ -62,7 +62,7 @@ class nicerAppCMS {
             trigger_error ('app.title.site.php missing for app='.json_encode($app), E_USER_ERROR);
         }
         
-        $getAsIndividualLinks = $this->domain==='nicerapp2';
+        $getAsIndividualLinks = $this->domain==='nicerapp';
         if ($getAsIndividualLinks) {
             $cssFiles = $this->getFiles_asIndividualLinks('css', 'css');
             $cssThemeFiles = $this->getFiles_asIndividualLinks('css', 'cssTheme');
