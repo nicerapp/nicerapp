6819664e9399181bc4b5e5b73131305ad68dbebe
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-16 12:21:12 +0200

    fixing opening and closing of nodes in blogging features tree

--- nicerapp/apps/nicerapp/cms/ajax_changeNodeStatus_openOrClosed.php
+++ nicerapp/apps/nicerapp/cms/ajax_changeNodeStatus_openOrClosed.php
@@ -2,7 +2,7 @@
 require_once (dirname(__FILE__).'/../../../boot.php');
 require_once (dirname(__FILE__).'/../../../3rd-party/sag/src/Sag.php');
 require_once (dirname(__FILE__).'/../../../Sag-support-functions.php');
-$debug = false;
+$debug = true;
 if ($debug) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
@@ -35,23 +35,12 @@ $cdb->login($cdbConfig['adminUsername'], $cdbConfig['adminPassword']);
 
 $cdb->setDatabase($_POST['database'],false);
 $call = $cdb->get ($_POST['id']);
-if ($debug) { echo '$call='; var_dump ($call); echo PHP_EOL.PHP_EOL; }
 $call->body->state->opened = $_POST['open']=='true' ? true : false;
+if ($debug) { echo '$call='; var_dump ($call); echo PHP_EOL.PHP_EOL; }
 
 try { $call = $cdb->post($call->body); } catch (Exception $e) {
     cdb_error (500, $e, 'Could not add record'); die();
 }
 
-$oldPath = realpath(dirname(__FILE__).'/../../../').'/siteData/nicerapp/'.$_POST['relFilePath'];
-$newPath = realpath(dirname(__FILE__).'/../../../').'/siteData/nicerapp/'.$_POST['newRelFilePath'];
-$xec = 'mv "'.$oldPath.'" "'.$newPath.'"';
-exec ($xec, $output, $result);
-$dbg = array (
-    'xec' => $xec,
-    'output' => $output,
-    'result' => $result
-);
-if ($debug) { echo '<pre style="color:green">'; var_dump ($dbg); echo '</pre>'; }
-
-echo 'Success'; // echo json_encode($recordToAdd); <-- not needed, js will refresh the entire tree (accounting for multiple users working on the same tree at the same time)
+echo 'Success'; 
 ?>
