ffe8298f3e3e678bc15fa5f6ae37d61f74312bc2
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-16 12:18:12 +0200

    fixing opening and closing of nodes in blogging features tree

--- nicerapp/apps/nicerapp/cms/na.blog.source.js
+++ nicerapp/apps/nicerapp/cms/na.blog.source.js
@@ -79,7 +79,6 @@ na.blog = {
                     na.blog.onchange_folderStatus_openOrClosed(e);
                     
                 }).on('changed.jstree', function (e, data) {
-                    debugger;
                     if (
                         na.blog.settings.current.selectedTreeNode
                         && na.blog.settings.current.selectedTreeNode.type=='naDocument'
