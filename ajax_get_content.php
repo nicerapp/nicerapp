0b9ba880f038435771af6c539013b104a106f63c
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-20 07:46:22 +0200

    some foldernames changed

--- ajax_get_content.php
+++ ajax_get_content.php
@@ -29,7 +29,8 @@
                     $filename = str_replace ($fileRoot, '', $filepath);
                     $dialogID = str_replace ('app.dialog.', '', $filename);
                     $dialogID = str_replace ('.php', '', $dialogID);
-                    $arr = array ( $dialogID => $filepath );
+                    if ($debug) { echo $filepath.'='; $filepath; echo PHP_EOL.PHP_EOL; die();}
+                    $arr = array ( $dialogID => execPHP($filepath) );
                     if ($debug) { echo $filepath.'='; var_dump ($arr); echo PHP_EOL.PHP_EOL; }
                     //$arr = array ( $dialogID => $filepath );
                     $ret = array_merge ($ret, $arr);
