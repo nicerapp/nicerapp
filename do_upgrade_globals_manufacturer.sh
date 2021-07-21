cdc83a5843c7b1a9d411b0569aa0bc2cb059b1eb
Rene AJM Veerman <rene.veerman.netherlands@gmail.com>
2021-07-21 09:38:28 +0200

    bugfixes

--- do_upgrade_globals_manufacturer.sh
+++ do_upgrade_globals_manufacturer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NA_MAIN_GROUP="www-data"
 NA_MAIN_USER="rene"
-NA_MAIN_PERMISSIONS=740
+NA_MAIN_PERMISSIONS=750 # PHP needs this +x permission to load scripts!
 NA_USERDATA_PERMISSIONS=760
 NA_MAIN_HTDOCS_RELATIVE_PATH="data1/htdocs"
 NA_MAIN_SITE_FOLDER="nicer.app"
